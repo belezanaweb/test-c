8f001785e324e26112466582adeac17e48797ffc
Felipe Moreti <felipe_moreti7@hotmail.com>
2019-05-24 20:42:30 -0300

    .

--- BelezaNaWeb/BelezaNaWeb/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace BelezaNaWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- BelezaNaWeb/BelezaNaWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BelezaNaWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
