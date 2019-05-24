25e8cc3848eb39f9f4b58056f590d3d5d3f7f2fc
Felipe Moreti <felipe_moreti7@hotmail.com>
2019-05-24 20:47:00 -0300

    files

---
+++ BelezaNaWeb/BelezaNaWeb/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BelezaNaWeb.Models;
+
+namespace BelezaNaWeb.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductsController : ApiController
+    {
+        List<ProductModel> listProducts = new List<ProductModel>();
+
+        // GET: api/Products/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BelezaNaWeb/BelezaNaWeb/Models/WarehouseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BelezaNaWeb.Models
+{
+    public class WarehouseModel
+    {
+        public string locality { get; set; }
+        public int quantity { get; set; }
+        public string type { get; set; }
+
+        public WarehouseModel() { }
+
+        public WarehouseModel(string locality, int quantity, string type)
+        {
+            this.locality = locality;
+            this.quantity = quantity;
+            this.type = type;
+        }
+    }
+}
