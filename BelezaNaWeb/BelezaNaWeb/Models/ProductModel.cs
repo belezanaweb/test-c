67840422fbbef38d27f5bb11e1b0b585fc28bdc6
Felipe Moreti <felipe_moreti7@hotmail.com>
2019-05-24 21:57:54 -0300

    Revert "."
    This reverts commit 8f001785e324e26112466582adeac17e48797ffc.

---
+++ BelezaNaWeb/BelezaNaWeb/Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BelezaNaWeb.Models
+{
+    public class ProductModel
+    {
+        public int skuCode { get; set; }
+        public string productName { get; set; }
+        public InventoryModel productInventory { get; set; }
+        public bool isMarketable
+        {
+            get
+            {
+                return (productInventory.quantity > 0);
+            }
+        }
+
+        public ProductModel() { }
+    }
+}
