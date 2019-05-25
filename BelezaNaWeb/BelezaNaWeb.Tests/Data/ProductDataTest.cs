6077bcb9a72394c25a58685e170fb9b4e6257598
Felipe Moreti <felipe_moreti7@hotmail.com>
2019-05-25 14:52:06 -0300

    add unit tests

---
+++ BelezaNaWeb/BelezaNaWeb.Tests/Data/ProductDataTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BelezaNaWeb;
+using BelezaNaWeb.Controllers;
+using BelezaNaWeb.Data;
+using BelezaNaWeb.Models;
+
+namespace BelezaNaWeb.Tests.Controllers
+{
+    [TestClass]
+    public class ProductDataTest
+    {
+        [TestMethod]
+        public void GetProducts()
+        {
+            ProductsData controller = new ProductsData();
+
+            List<WarehouseModel> warehouseList = new List<WarehouseModel>();
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            controller.Add(new ProductModel(1, "teste", new InventoryModel(warehouseList)));
+            controller.Add(new ProductModel(2, "outro", new InventoryModel(warehouseList)));
+
+            List<ProductModel> result = controller.GetProducts();
+            
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void GetProductById()
+        {
+            ProductsData controller = new ProductsData();
+
+            List<WarehouseModel> warehouseList = new List<WarehouseModel>();
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            ProductModel prod = new ProductModel(4, "teste", new InventoryModel(warehouseList));
+            controller.Add(prod);
+
+            ProductModel result = controller.GetProductBySKU(4);
+            
+            Assert.IsNotNull(result);
+            Assert.AreEqual(prod, result);
+        }
+
+        [TestMethod]
+        public void AddProduct()
+        {
+            ProductsData controller = new ProductsData();
+
+            List<WarehouseModel> warehouseList = new List<WarehouseModel>();
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            ProductModel prod = new ProductModel(3, "teste", new InventoryModel(warehouseList));
+            controller.Add(prod);
+
+            ProductModel result = controller.GetProductBySKU(3);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(prod, result);
+        }
+
+        [TestMethod]
+        public void ModifyProduct()
+        {
+            ProductsData controller = new ProductsData();
+
+            List<WarehouseModel> warehouseList = new List<WarehouseModel>();
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            ProductModel prod = new ProductModel(5, "teste", new InventoryModel(warehouseList));
+            controller.Add(prod);
+
+            prod.name = "teste modificado";
+            controller.ModifyProduct(5, prod);
+
+            ProductModel result = controller.GetProductBySKU(5);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("teste modificado", result.name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),
+            "Produto não encontrado")]
+        public void RemoveProduct()
+        {
+            ProductsData controller = new ProductsData();
+
+            List<WarehouseModel> warehouseList = new List<WarehouseModel>();
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            warehouseList.Add(new WarehouseModel("SP", 5, "WEB"));
+            controller.Add(new ProductModel(6, "teste", new InventoryModel(warehouseList)));
+            controller.RemoveProduct(6);
+
+            ProductModel result = controller.GetProductBySKU(6);
+        }
+    }
+}
---
+++ BelezaNaWeb/BelezaNaWeb.Tests/Models/ProductTest.cs
@@ -0,0 +1,66 @@
+﻿using BelezaNaWeb.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelezaNaWeb.Tests.Controllers
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void GetQuantity()
+        {
+            List<WarehouseModel> warehouses = new List<WarehouseModel>();
+            warehouses.Add(new WarehouseModel("SP", 3, "WEB"));
+            warehouses.Add(new WarehouseModel("RJ", 5, "WEB"));
+            InventoryModel inventory = new InventoryModel(warehouses);
+            ProductModel product = new ProductModel(1, "test quantity", inventory);
+
+            int result = product.inventory.quantity;
+
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void IsMarketable()
+        {
+            List<WarehouseModel> warehouses = new List<WarehouseModel>();
+            warehouses.Add(new WarehouseModel("SP", 3, "WEB"));
+            warehouses.Add(new WarehouseModel("RJ", 5, "WEB"));
+            InventoryModel inventory = new InventoryModel(warehouses);
+            ProductModel product = new ProductModel(1, "test quantity", inventory);
+
+            bool result = product.isMarketable;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetZeroQuantity()
+        {
+            List<WarehouseModel> warehouses = new List<WarehouseModel>();
+            InventoryModel inventory = new InventoryModel(warehouses);
+            ProductModel product = new ProductModel(1, "test quantity", inventory);
+
+            int result = product.inventory.quantity;
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void IsMarketableFalse()
+        {
+            List<WarehouseModel> warehouses = new List<WarehouseModel>();
+            InventoryModel inventory = new InventoryModel(warehouses);
+            ProductModel product = new ProductModel(1, "test quantity", inventory);
+
+            bool result = product.isMarketable;
+
+            Assert.IsFalse(result);
+        }
+    }
+}
