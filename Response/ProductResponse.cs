57022a1cb33a416e824060ceeb90f751c67945bd
ruiribeiro89 <ribeiro.rui89@gmail.com>
2020-09-28 20:40:31 -0300

    Teste Boticario

---
+++ Response/ProductResponse.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory.api.Response
+{
+    public class ProductResponse
+    {
+        public int sku { get; set; }
+        public string name { get; set; }
+        public InventoryProduct inventory { get; set; }
+        
+    }
+}
