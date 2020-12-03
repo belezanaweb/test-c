03579cae79fbe1c83e37ef8df3d0b5e1ab66cd47
brunotragl <bruno@DESKTOP-QKFQPF9>
2020-12-03 14:44:19 -0300

    implemented mapping between models

---
+++ src/BrunoTragl.BelezaNaWeb/BrunoTragl.BelezaNaWeb.Web.WebApi/Configurations/AutoMapperConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BrunoTragl.BelezaNaWeb.Domain.Model;
+using BrunoTragl.BelezaNaWeb.Web.WebApi.Models;
+
+namespace BrunoTragl.BelezaNaWeb.Web.WebApi.Configurations
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<Product, ProductModel>().ReverseMap();
+            CreateMap<Inventory, InventoryModel>().ReverseMap();
+            CreateMap<Warehouse, WarehouseModel>().ReverseMap();
+        }
+    }
+}
