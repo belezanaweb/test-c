f6115f05b55aa20744e9d0db775fc26422d3b770
brunotragl <bruno@DESKTOP-QKFQPF9>
2020-12-03 11:59:40 -0300

    start on Tests project

---
+++ src/BrunoTragl.BelezaNaWeb/BrunoTragl.BelezaNaWeb.Tests.WebApi/Base/WebApiBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BrunoTragl.BelezaNaWeb.Tests.WebApi.Base
+{
+    public abstract class WebApiBase
+    {
+        protected readonly HttpClient _httpClient;
+
+        public WebApiBase()
+        {
+            _httpClient = CreateServerAndClient();
+        }
+
+        protected virtual HttpClient CreateServerAndClient()
+        {
+            return new HttpClient();
+        }
+    }
+}
---
+++ src/BrunoTragl.BelezaNaWeb/BrunoTragl.BelezaNaWeb.Tests.WebApi/SuccessWebApiTest.cs
@@ -0,0 +1,33 @@
+using BrunoTragl.BelezaNaWeb.Tests.WebApi.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrunoTragl.BelezaNaWeb.Tests.WebApi
+{
+    [TestClass]
+    public class SuccessWebApiTest : WebApiBase
+    {
+        [TestMethod]
+        public void GetProductTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PostProductTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PutProductTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteProductTest()
+        {
+
+        }
+    }
+}
