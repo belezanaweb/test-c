d272bcc93785eb56c8baa2c0ea79439c418866b2
brunotragl <bruno@DESKTOP-QKFQPF9>
2020-12-03 12:16:09 -0300

    context data created

---
+++ src/BrunoTragl.BelezaNaWeb/BrunoTragl.BelezaNaWeb.Infra.Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using BrunoTragl.BelezaNaWeb.Domain.Model;
+using BrunoTragl.BelezaNaWeb.Infra.Data.Interfaces;
+using System.Collections.Generic;
+
+namespace BrunoTragl.BelezaNaWeb.Infra.Data
+{
+    public class Context : IContext
+    {
+        public Context()
+        {
+            Products = new List<Product>();
+        }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ src/BrunoTragl.BelezaNaWeb/BrunoTragl.BelezaNaWeb.Infra.Data/Interfaces/IContext.cs
@@ -0,0 +1,10 @@
+﻿using BrunoTragl.BelezaNaWeb.Domain.Model;
+using System.Collections.Generic;
+
+namespace BrunoTragl.BelezaNaWeb.Infra.Data.Interfaces
+{
+    public interface IContext
+    {
+        ICollection<Product> Products { get; set; }
+    }
+}
