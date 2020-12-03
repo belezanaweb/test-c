8521f187acca98ec5c1989784d2123aa0f635a03
brunotragl <bruno@DESKTOP-QKFQPF9>
2020-12-03 12:30:41 -0300

    repository pattern of product created

---
+++ src/BrunoTragl.BelezaNaWeb/BrunoTragl.BelezaNaWeb.Domain.Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using BrunoTragl.BelezaNaWeb.Domain.Model;
+
+namespace BrunoTragl.BelezaNaWeb.Domain.Repository.Interfaces
+{
+    public interface IProductRepository
+    {
+        Product Get(long sku);
+        void Add(Product product);
+        void Update(Product product);
+        void Remove(long sku);
+        bool Any(long sku);
+    }
+}
---
+++ src/BrunoTragl.BelezaNaWeb/BrunoTragl.BelezaNaWeb.Domain.Repository/ProductRepository.cs
@@ -0,0 +1,75 @@
+﻿using BrunoTragl.BelezaNaWeb.Domain.Model;
+using BrunoTragl.BelezaNaWeb.Domain.Repository.Interfaces;
+using BrunoTragl.BelezaNaWeb.Infra.Data.Interfaces;
+using System;
+using System.Linq;
+
+namespace BrunoTragl.BelezaNaWeb.Domain.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IContext _context;
+        public ProductRepository(IContext context)
+        {
+            _context = context;
+        }
+        public Product Get(long sku)
+        {
+            try
+            {
+                return _context.Products.FirstOrDefault(p => p.Sku == sku);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool Any(long sku)
+        {
+            try
+            {
+                return _context.Products.Any(p => p.Sku == sku);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Add(Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Update(Product product)
+        {
+            try
+            {
+                Product model = _context.Products.FirstOrDefault(p => p.Sku == product.Sku);
+                model.Name = product.Name;
+                model.Inventory = model.Inventory;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Remove(long sku)
+        {
+            try
+            {
+                Product product = _context.Products.FirstOrDefault(p => p.Sku == sku);
+                _context.Products.Remove(product);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
