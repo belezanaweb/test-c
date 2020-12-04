342aef908b516922fc8325eba0b9d74b80b84b54
Tauan Matos <tauan.matos@warren.com.br>
2020-12-04 12:18:22 -0300

    services

---
+++ src/BelezaNaWeb.Services/Interfaces/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using BelezaNaWeb.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BelezaNaWeb.Services.Interfaces
+{
+    public interface IProductService
+    {
+        IList<Product> GetAll();
+        Product Find(int sku);
+        void Update(int sku, Product product);
+        void Delete(int sku);
+        void Insert(Product newProduct);
+    }
+}
---
+++ src/BelezaNaWeb.Services/Services/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using BelezaNaWeb.Data.Models;
+using BelezaNaWeb.Data.Repositories.Interfaces;
+using BelezaNaWeb.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace BelezaNaWeb.Services.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IList<Product> GetAll() => _productRepository.Select();
+
+        public Product Find(int sku) => _productRepository.Select(sku);
+
+        public void Update(int sku, Product newProduct)
+        {
+            var product = _productRepository.Select(sku);
+            if (product is null) throw new Exception($"O produto de SKU {sku} não foi encontrado.");
+
+            product.Name = newProduct.Name;
+            product.Inventory = newProduct.Inventory;
+
+            _productRepository.Update(product);
+        }
+        
+        public void Delete(int sku)
+        {
+            var product = _productRepository.Select(sku);
+            if (product is null) throw new Exception($"O produto de SKU {sku} não foi encontrado.");
+
+            _productRepository.Delete(sku);
+        }
+
+        public void Insert(Product newProduct)
+        {
+            var product = _productRepository.Select(newProduct.Sku);
+            if (product != null) throw new Exception($"O produto de SKU {product.Sku} já existe na base.");
+
+            _productRepository.Insert(newProduct);
+        }
+
+
+
+    }
+}
