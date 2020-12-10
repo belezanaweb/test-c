6abe34a05b6c1c85d798c7ed238ec7c0e95322cf
GUILHERME HELAEHIL <guilherme.helaehil@gmail.com>
2020-12-10 20:54:13 -0300

    Greetings text changes

--- TesteGuilhermeHelaehil/TesteGuilhermeHelaehil/Controllers/BoasVindasController.cs
+++ TesteGuilhermeHelaehil/TesteGuilhermeHelaehil/Controllers/BoasVindasController.cs
@@ -12,19 +12,20 @@ namespace TesteGuilhermeHelaehil.Controllers
     public class BoasVindasController
     {
         // GET: api/product
-        // endpoint pras boas vindas
+        // endpoint para as boas vindas
         [HttpGet]
         public string Get()
         {
-            return "\t Bem vindos ao meu teste, espero que gostem do que  \n" +
-                   "verão, dei o meu melhor porem  tenho mais a oferecer do que foi \n" +
-                   "abordado nessa solução, infelizmente como ainda estou aprendendo \n" +
-                   "a testar, não consegui terminar os teste de integração, porem \n" +
-                   "estou empolgado para aprender mais sobre. \n\n" +
-                   "\t Tomei a iniciatica de montar uma collection(v2.1) no postman \n" +
+            return "\t Bem vindos ao meu teste, espero que gostem do que \n" +
+                   "irão ver, dei o meu melhor, porém tenho mais a oferecer do que foi \n" +
+                   "abordado nessa solução. Infelizmente como ainda estou aprendendo \n" +
+                   "testes automatizados, não consegui terminar os teste de integração,\n" +
+                   "entretanto estou empolgado para aprender mais sobre. \n\n" +
+                   "\t Tomei a iniciativa de montar uma collection(v2.1) no Postman \n" +
                    "para facilitar a utilização dessa API a Collection está localizada\n" +
                    "no arquivo: \\test-c\\Teste Guilherme Helaehil.postman_collection.json.\n\n" +
                    "\t Obrigado pela oportunidade e aguardo um retorno :D ";
+
         }
     }
 }
